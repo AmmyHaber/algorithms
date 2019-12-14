4d32fd540631a729bb50856fc2753ee3eb006994
ammyhaber <ammyhaber@outlook.com>
2019-12-14 22:19:04 +0800

    commit RecursiveBinarySearch

---
+++ src/com/ammyhaber/algorithms/search/RecursiveBinarySearch.java
@@ -0,0 +1,38 @@
+package com.ammyhaber.algorithms.search;
+
+import com.ammyhaber.algorithms.sort.RandomArrayMaker;
+import com.ammyhaber.algorithms.sort.insertion.InsertionSort;
+
+import java.util.Arrays;
+
+/**
+ * @author ammyhaber
+ * @version 1.0
+ * @date 2019/12/14
+ */
+public class RecursiveBinarySearch {
+
+    public static void main(String[] args) {
+        int[] a = RandomArrayMaker.make(16);
+        InsertionSort.insertionSort(a);
+        System.out.println(Arrays.toString(a));
+        int b = a[11];
+        int i = binarySearch(a, 0, a.length - 1, b);
+        if (i == -1)
+            System.out.println(String.format("%d is not in the array", b));
+        else
+            System.out.println(String.format("Search %d from array a, the index:%d", b, i));
+    }
+
+    private static int binarySearch(int[] a, int l, int r, int b) {
+        if (l > r)
+            return -1;
+        int m = (l + r) / 2;
+        if (a[m] == b)
+            return m;
+        else if (a[m] > b)
+            return binarySearch(a, l, m - 1, b);
+        else
+            return binarySearch(a, m + 1, r, b);
+    }
+}
