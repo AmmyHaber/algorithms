92f7ef22d2e3354461e666a4db479a1daac4eb96
wangzhaori <wangzhaori@linklogis.com>
2021-08-06 11:53:03 +0800

    commit ListNode.java and ListNodeUtils.java

---
+++ src/com/ammyhaber/leetcode/ListNodeUtils.java
@@ -0,0 +1,23 @@
+package com.ammyhaber.leetcode;
+
+public class ListNodeUtils {
+
+    public static ListNode createSimple() {
+        return new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+    }
+
+    public static void print(ListNode head) {
+        if (head == null) {
+            System.out.println("[]");
+            return;
+        }
+        ListNode n = head;
+        StringBuilder sb = new StringBuilder("[");
+        while (n != null) {
+            sb.append(n.val).append(", ");
+            n = n.next;
+        }
+        
+        System.out.println(sb.substring(0, sb.length() - 2) + "]");
+    }
+}
