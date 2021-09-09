420dd2b0b8f83d17c6a05e88bc452e72262cbc7d
wangzhaori <wangzhaori@linklogis.com>
2021-09-09 16:34:46 +0800

    commit Leetcode3

--- src/com/ammyhaber/leetcode/Leetcode3.java
+++ src/com/ammyhaber/leetcode/Leetcode3.java
@@ -16,23 +16,20 @@ public class Leetcode3 {
     }
 
     public static int lengthOfLongestSubstring(String s) {
-        // 哈希集合，记录每个字符是否出现过
-        Set<Character> occ = new HashSet<>();
-        int n = s.length();
-        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        HashSet<Character> occ = new HashSet<>();
+
         int rk = -1, ans = 0;
-        for (int i = 0; i < n; ++i) {
-            if (i != 0) {
-                // 左指针向右移动一格，移除一个字符
-                occ.remove(s.charAt(i - 1));
+        for (int i = 0; i < s.length(); i++) {
+            if (i > 0) {
+            occ.remove(s.charAt(i - 1));
             }
-            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
-                // 不断地移动右指针
+
+            while (rk + 1 < s.length() && !occ.contains(s.charAt(rk + 1))) {
                 occ.add(s.charAt(rk + 1));
-                ++rk;
+                rk++;
             }
-            // 第 i 到 rk 个字符是一个极长的无重复字符子串
-            ans = Math.max(ans, rk - i + 1);
+
+            ans = Math.max(ans, rk + 1 - i);
         }
         return ans;
     }
