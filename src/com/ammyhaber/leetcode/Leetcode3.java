89dfc990a1d382f83c3fe863be51aadc658890ab
wangzhaori <wangzhaori@linklogis.com>
2021-09-09 16:20:33 +0800

    commit Leetcode3

---
+++ src/com/ammyhaber/leetcode/Leetcode3.java
@@ -0,0 +1,39 @@
+package com.ammyhaber.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 3. 无重复字符的最长子串
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+ */
+public class Leetcode3 {
+
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        int length = lengthOfLongestSubstring(s);
+        assert length == 3;
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        // 哈希集合，记录每个字符是否出现过
+        Set<Character> occ = new HashSet<>();
+        int n = s.length();
+        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        int rk = -1, ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                // 左指针向右移动一格，移除一个字符
+                occ.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                // 不断地移动右指针
+                occ.add(s.charAt(rk + 1));
+                ++rk;
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = Math.max(ans, rk - i + 1);
+        }
+        return ans;
+    }
+}
