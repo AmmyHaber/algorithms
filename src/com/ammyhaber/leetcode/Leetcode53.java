d72a0c57ddc95998988af2ca06f66311b9eb1be2
ammyhaber <ammyhaber@outlook.com>
2021-10-25 22:55:01 +0800

    commit Leetcode53

---
+++ src/com/ammyhaber/leetcode/Leetcode53.java
@@ -0,0 +1,17 @@
+package com.ammyhaber.leetcode;
+
+/**
+ * 53. 最大子序和
+ *
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ */
+public class Leetcode53 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int maxSubArray(int[] nums) {
+        return 0;
+    }
+}
