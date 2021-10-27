da849bbb6d7d34412a9ff06933d86d251803895b
wangzhaori <wangzhaori@linklogis.com>
2021-10-27 19:50:09 +0800

    commit Leetcode11

---
+++ src/com/ammyhaber/leetcode/Leetcode11.java
@@ -0,0 +1,20 @@
+package com.ammyhaber.leetcode;
+
+/**
+ * 11. 盛最多水的容器
+ * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点(i,ai) 。在坐标内画 n 条垂直线，垂直线 i的两个端点分别为(i,ai) 和 (i, 0) 。
+ * 找出其中的两条线，使得它们与x轴共同构成的容器可以容纳最多的水。
+ *
+ * 说明：你不能倾斜容器。
+ *
+ */
+public class Leetcode11 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int maxArea(int[] height) {
+        return 0;
+    }
+}
