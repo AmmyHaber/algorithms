c424385fbfb93d5081e8be28293cd90215e11a0a
wangzhaori <wangzhaori@linklogis.com>
2021-11-01 15:13:17 +0800

    commit Leetcode70

---
+++ src/com/ammyhaber/leetcode/Leetcode70.java
@@ -0,0 +1,26 @@
+package com.ammyhaber.leetcode;
+
+/**
+ * 70. 爬楼梯
+ * <p>
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 注意：给定 n 是一个正整数。
+ */
+public class Leetcode70 {
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(16534876));
+    }
+
+    public static int climbStairs(int n) {
+        int p,q=0,r=1;
+        for (int i = 1; i <= n; i++) {
+            p = q;
+            q = r;
+            r = p + q;
+        }
+        return r;
+    }
+
+}
