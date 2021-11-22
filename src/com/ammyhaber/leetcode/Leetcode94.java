513208323f8393fe5e36d2464afaf171dee79d2a
wangzhaori <wangzhaori@linklogis.com>
2021-11-22 19:42:41 +0800

    commit Leetcode94

---
+++ src/com/ammyhaber/leetcode/Leetcode94.java
@@ -0,0 +1,37 @@
+package com.ammyhaber.leetcode;
+
+import java.util.List;
+
+/**
+ * 94. 二叉树的中序遍历
+ * 给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+ */
+public class Leetcode94 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        return null;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
