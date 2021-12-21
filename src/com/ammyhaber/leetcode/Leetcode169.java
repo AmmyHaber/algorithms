cfb129fdd5863cbedc41f05c76d98b4fdb239ac7
wangzhaori <wangzhaori@linklogis.com>
2021-12-21 20:26:22 +0800

    commit Leetcode169

---
+++ src/com/ammyhaber/leetcode/Leetcode169.java
@@ -0,0 +1,19 @@
+package com.ammyhaber.leetcode;
+
+/**
+ * 169. 多数元素
+ * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+ *
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ *
+ */
+public class Leetcode169 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int majorityElement(int[] nums) {
+        return 0;
+    }
+}
