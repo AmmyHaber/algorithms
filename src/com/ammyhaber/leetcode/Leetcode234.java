08fc0a89d6fb9be0a0181735f89b958ad0d8299b
wangzhaori <wangzhaori@linklogis.com>
2021-12-22 12:01:24 +0800

    commit Leetcode234

---
+++ src/com/ammyhaber/leetcode/Leetcode234.java
@@ -0,0 +1,16 @@
+package com.ammyhaber.leetcode;
+
+/**
+ * 234. 回文链表
+ * 给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。
+ */
+public class Leetcode234 {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(ListNodeUtils.create(1, 2, 2, 1)));;
+    }
+
+    public static boolean isPalindrome(ListNode head) {
+        return false;
+    }
+}
