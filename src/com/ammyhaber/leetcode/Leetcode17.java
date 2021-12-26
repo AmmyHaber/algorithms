2c85b252cc88b71c920a9e8f981a14909fdee549
wangzhaori <wangzhaori@linklogis.com>
2021-12-26 22:00:29 +0800

    commit Leetcode17

---
+++ src/com/ammyhaber/leetcode/Leetcode17.java
@@ -0,0 +1,24 @@
+package com.ammyhaber.leetcode;
+
+import java.util.List;
+
+/**
+ * 17. 电话号码的字母组合
+ *
+ * 给定一个仅包含数字2-9的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+ *
+ * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+ *
+ */
+public class Leetcode17 {
+
+    public static void main(String[] args) {
+        System.out.println(letterCombinations("23"));
+    }
+
+    public static List<String> letterCombinations(String digits) {
+        return null;
+    }
+
+
+}
