9cbf5c1a47d81afc292ddeee45dffc8552ea1a2e
wangzhaori <wangzhaori@linklogis.com>
2021-12-29 16:13:30 +0800

    rename

--- src/com/ammyhaber/leetcode/Leetcode15_.java
+++ src/com/ammyhaber/leetcode/Leetcode15_.java
@@ -11,7 +11,7 @@ import java.util.List;
  * 注意：答案中不可以包含重复的三元组。
  *
  */
-public class Leetcode15 {
+public class Leetcode15_ {
 
     public static void main(String[] args) {
         int[] nums = new int[]{-1,0,1,2,-1,-4};
--- src/com/ammyhaber/leetcode/Leetcode22_.java
+++ src/com/ammyhaber/leetcode/Leetcode22_.java
@@ -8,7 +8,7 @@ import java.util.List;
  * <p>
  * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
  */
-public class Leetcode22 {
+public class Leetcode22_ {
 
     public static void main(String[] args) {
         System.out.println(generateParenthesis(3));
