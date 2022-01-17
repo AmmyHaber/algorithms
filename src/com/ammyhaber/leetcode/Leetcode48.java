34ca3ac424cce5bd2ddb13e981e4edfc69f750c6
wangzhaori <wangzhaori@linklogis.com>
2022-01-17 18:43:58 +0800

    commit Leetcode48

--- src/com/ammyhaber/leetcode/Leetcode48.java
+++ src/com/ammyhaber/leetcode/Leetcode48.java
@@ -1,12 +1,17 @@
 package com.ammyhaber.leetcode;
 
+import java.util.Arrays;
+
 public class Leetcode48 {
 
     public static void main(String[] args) {
-
+        int[][] matrix = new int[][]{new int[]{1, 2, 3}, new int[]{4, 5, 6}, new int[]{7, 8, 9}};
+        System.out.println(Arrays.toString(matrix));
+        rotate(matrix);
+        System.out.println(Arrays.toString(matrix));
     }
 
-    public void rotate(int[][] matrix) {
+    public static void rotate(int[][] matrix) {
 
     }
 }
