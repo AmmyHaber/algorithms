b82607158ee9e9576a106b7443d1089b8d1149df
wangzhaori <wangzhaori@linklogis.com>
2022-01-19 18:15:14 +0800

    commit Leetcode55

---
+++ src/com/ammyhaber/leetcode/Leetcode55.java
@@ -0,0 +1,14 @@
+package com.ammyhaber.leetcode;
+
+public class Leetcode55 {
+
+    public static void main(String[] args) {
+        System.out.println(canJump(new int[]{2, 3, 1, 1, 4}));
+        System.out.println(canJump(new int[]{3, 2, 1, 0, 4}));
+        System.out.println(canJump(new int[]{0}));
+    }
+
+    public static boolean canJump(int[] nums) {
+        return false;
+    }
+}
