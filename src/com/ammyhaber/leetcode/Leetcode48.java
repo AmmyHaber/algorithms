53880d76299cdade3bbf4b033f3c8f661c9a345e
wangzhaori <wangzhaori@linklogis.com>
2022-01-19 17:11:50 +0800

    commit Leetcode48

--- src/com/ammyhaber/leetcode/Leetcode48.java
+++ src/com/ammyhaber/leetcode/Leetcode48.java
@@ -12,6 +12,15 @@ public class Leetcode48 {
     }
 
     public static void rotate(int[][] matrix) {
-
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < (n - n / 2); j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = tmp;
+            }
+        }
     }
 }
