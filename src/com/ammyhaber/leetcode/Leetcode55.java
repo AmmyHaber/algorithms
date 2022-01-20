c7f853d03dedea3fc8c71c7d7fb49a81370f2a1b
wangzhaori <wangzhaori@linklogis.com>
2022-01-20 10:43:04 +0800

    commit Leetcode55

--- src/com/ammyhaber/leetcode/Leetcode55.java
+++ src/com/ammyhaber/leetcode/Leetcode55.java
@@ -9,6 +9,13 @@ public class Leetcode55 {
     }
 
     public static boolean canJump(int[] nums) {
-        return false;
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                return false;
+            }
+            k = Math.max(k, i + nums[i]);
+        }
+        return true;
     }
 }
