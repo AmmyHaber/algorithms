72d6269be6932454bdf8c13642e07513f1734a38
wangzhaori <wangzhaori@linklogis.com>
2022-02-10 15:37:09 +0800

    commit Leetcode240

---
+++ src/com/ammyhaber/leetcode/Leetcode240_.java
@@ -0,0 +1,33 @@
+package com.ammyhaber.leetcode;
+
+public class Leetcode240_ {
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+        System.out.println(searchMatrix(matrix, 5));
+        int[][] matrix2 = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+        System.out.println(searchMatrix(matrix2, 20));
+        int[][] matrix3 = {{1, 1}};
+        System.out.println(searchMatrix(matrix3, 2));
+        int[][] matrix4 = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
+        System.out.println(searchMatrix(matrix4, 15));
+    }
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int x = 0;
+        int y = n - 1;
+        while (x < m && y >= 0) {
+            if (matrix[x][y] == target) {
+                return true;
+            } else if (matrix[x][y] > target) {
+                y--;
+            } else {
+                x++;
+            }
+        }
+        return false;
+    }
+}
