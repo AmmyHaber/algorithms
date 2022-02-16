eeef0298664c4a36b008c6b2bb218482301df116
wangzhaori <wangzhaori@linklogis.com>
2022-02-16 15:54:44 +0800

    commit Leetcode560

---
+++ src/com/ammyhaber/leetcode/Leetcode560_.java
@@ -0,0 +1,26 @@
+package com.ammyhaber.leetcode;
+
+import java.util.HashMap;
+
+public class Leetcode560_ {
+
+    public static void main(String[] args) {
+        System.out.println(subarraySum(new int[]{0, -1, 0, 3, 1, 12, -13, 7, 2}, 2));
+        System.out.println(subarraySum(new int[]{1, 1, 1}, 2));
+        System.out.println(subarraySum(new int[]{1, 2, 3}, 3));
+    }
+
+    public static int subarraySum(int[] nums, int k) {
+        int count = 0, pre = 0;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        mp.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            pre += nums[i];
+            if (mp.containsKey(pre - k)) {
+                count += mp.get(pre - k);
+            }
+            mp.put(pre, mp.getOrDefault(pre, 0) + 1);
+        }
+        return count;
+    }
+}
