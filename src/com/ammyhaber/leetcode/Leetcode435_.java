fd35e9c02f7a6b75ca3bedb35e796937cea38aaa
wangzhaori <wangzhaori@linklogis.com>
2022-02-21 18:55:35 +0800

    commit Leetcode435

---
+++ src/com/ammyhaber/leetcode/Leetcode435_.java
@@ -0,0 +1,31 @@
+package com.ammyhaber.leetcode;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Leetcode435_ {
+
+    public static void main(String[] args) {
+        System.out.println(eraseOverlapIntervals(new int[][]{{1, 2}, {2, 3}, {3, 4}, {1, 3}}));
+    }
+
+    public static int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[1]));
+
+        int n = intervals.length;
+        int right = intervals[0][1];
+        int ans = 1;
+        for (int i = 1; i < n; ++i) {
+            if (intervals[i][0] >= right) {
+                ++ans;
+                right = intervals[i][1];
+            }
+        }
+        return n - ans;
+    }
+
+}
