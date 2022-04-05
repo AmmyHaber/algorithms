4638bf5870824e2750c587bf5e645b350ff42b10
ammyhaber <ammyhaber@outlook.com>
2022-04-05 22:47:04 +0800

    commit Leetcode1262

---
+++ src/com/ammyhaber/leetcode/Leetcode1262.java
@@ -0,0 +1,14 @@
+package com.ammyhaber.leetcode;
+
+public class Leetcode1262 {
+
+    public static void main(String[] args) {
+        System.out.println(maxSumDivThree(new int[]{3,6,5,1,8}));
+        System.out.println(maxSumDivThree(new int[]{4}));
+        System.out.println(maxSumDivThree(new int[]{1,2,3,4,4}));
+    }
+
+    public static int maxSumDivThree(int[] nums) {
+        return 0;
+    }
+}
