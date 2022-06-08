bda1d245cf32138f3d008a58805a84422c207ab0
wangzhaori <wangzhaori@linklogis.com>
2022-06-08 13:53:49 +0800

    commit Leetcode678

---
+++ src/com/ammyhaber/leetcode/Leetcode678_.java
@@ -0,0 +1,98 @@
+package com.ammyhaber.leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Leetcode678_ {
+
+    public static void main(String[] args) {
+        System.out.println(checkValidString("()"));
+        System.out.println(checkValidString("(*)"));
+        System.out.println(checkValidString("(*))"));
+    }
+
+    /**
+     * 动态规划
+     */
+    public static boolean checkValidString1(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '*') {
+                dp[i][i] = true;
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            char c1 = s.charAt(i - 1), c2 = s.charAt(i);
+            dp[i - 1][i] = (c1 == '(' || c1 == '*') && (c2 == ')' || c2 == '*');
+        }
+        for (int i = n - 3; i >= 0; i--) {
+            char c1 = s.charAt(i);
+            for (int j = i + 2; j < n; j++) {
+                char c2 = s.charAt(j);
+                if ((c1 == '(' || c1 == '*') && (c2 == ')' || c2 == '*')) {
+                    dp[i][j] = dp[i + 1][j - 1];
+                }
+                for (int k = i; k < j && !dp[i][j]; k++) {
+                    dp[i][j] = dp[i][k] && dp[k + 1][j];
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+
+    /**
+     * 栈
+     */
+    public static boolean checkValidString2(String s) {
+        Deque<Integer> leftStack = new LinkedList<>();
+        Deque<Integer> asteriskStack = new LinkedList<>();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                leftStack.push(i);
+            } else if (c == '*') {
+                asteriskStack.push(i);
+            } else {
+                if (!leftStack.isEmpty()) {
+                    leftStack.pop();
+                } else if (!asteriskStack.isEmpty()) {
+                    asteriskStack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        while (!leftStack.isEmpty() && !asteriskStack.isEmpty()) {
+            int leftIndex = leftStack.pop();
+            int asteriskIndex = asteriskStack.pop();
+            if (leftIndex > asteriskIndex) {
+                return false;
+            }
+        }
+        return leftStack.isEmpty();
+    }
+
+    public static boolean checkValidString(String s) {
+        int minCount = 0, maxCount = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                minCount++;
+                maxCount++;
+            } else if (c == ')') {
+                minCount = Math.max(minCount - 1, 0);
+                maxCount--;
+                if (maxCount < 0) {
+                    return false;
+                }
+            } else {
+                minCount = Math.max(minCount - 1, 0);
+                maxCount++;
+            }
+        }
+        return minCount == 0;
+    }
+}
