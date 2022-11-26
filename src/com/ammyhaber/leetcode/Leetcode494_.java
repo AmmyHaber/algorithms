e1b2dd0979917d572bd33ec09f3af7cb558f027e
wangzhaori <wangzhaori@linklogis.com>
2022-11-26 17:15:37 +0800

    commit Leetcode494_.java

--- src/com/ammyhaber/leetcode/Leetcode494_.java
+++ src/com/ammyhaber/leetcode/Leetcode494_.java
@@ -7,6 +7,9 @@ public class Leetcode494_ {
         System.out.println(findTargetSumWays(new int[]{1, 1, 1, 1, 1}, 3));
     }
 
+    /**
+     * 回溯
+     */
     public static int findTargetSumWays(int[] nums, int target) {
         backtrack(nums, target, 0, 0);
         return count;
@@ -22,5 +25,32 @@ public class Leetcode494_ {
             backtrack(nums, target, index + 1, sum + nums[index]);
             backtrack(nums, target, index + 1, sum - nums[index]);
         }
+    }
+
+    /**
+     * 动态规划
+     */
+    public static int findTargetSumWays2(int[] nums, int target) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int diff = sum - target;
+        if (diff < 0 || diff % 2 != 0) {
+            return 0;
+        }
+        int n = nums.length, neg = diff / 2;
+        int[][] dp = new int[n + 1][neg + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            int num = nums[i - 1];
+            for (int j = 0; j <= neg; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= num) {
+                    dp[i][j] += dp[i - 1][j - num];
+                }
+            }
+        }
+        return dp[n][neg];
     }
 }
