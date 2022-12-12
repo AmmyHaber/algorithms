846bf011e56cd650c1394d195271411289104966
wangzhaori <wangzhaori@linklogis.com>
2022-12-12 22:29:43 +0800

    commit Leetcode287

---
+++ src/com/ammyhaber/leetcode/Leetcode287.java
@@ -0,0 +1,23 @@
+package com.ammyhaber.leetcode;
+
+public class Leetcode287 {
+
+    public static void main(String[] args) {
+        System.out.println(findDuplicate(new int[]{1, 3, 4, 2, 2}));
+        System.out.println(findDuplicate(new int[]{3, 1, 3, 4, 2}));
+    }
+
+    public static int findDuplicate(int[] nums) {
+        int slow = 0, fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
