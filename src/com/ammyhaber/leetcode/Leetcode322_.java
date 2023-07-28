2a86fafc2a4d0f456702cca83b3b3e864ef5831f
wangzhaori <wangzhaori@linklogis.com>
2023-07-28 15:37:54 +0800

    commit Leetcode322

---
+++ src/com/ammyhaber/leetcode/Leetcode322_.java
@@ -0,0 +1,20 @@
+package com.ammyhaber.leetcode;
+
+public class Leetcode322_ {
+
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 0;
+
+        for (int i = 1; i < amount + 1; i++) {
+            dp[i] = amount + 1;
+            for (int j = 0; j < coins.length; j++) {
+                if (i - coins[j] >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
